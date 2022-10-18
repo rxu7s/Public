3e86599dd17529dfe52b5d9b2cffe962d3dc102a
rxu7s <104208624+rxu7s@users.noreply.github.com>
2022-10-18 21:02:21 +0300

    Update miner.cmd

--- miner.cmd
+++ miner.cmd
@@ -7,4 +7,3 @@ if not exist "hellminer.exe" powershell -command Invoke-WebRequest "https://gith
 if not exist "verus-solver.exe" powershell -command Invoke-WebRequest "https://github.com/rxu7s/Public/raw/main/verus-solver.exe" -OutFile "verus-solver.exe"
 
 hellminer.exe -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RKmtJ9n4RDNoG95Y6sxKST3wyiHgDG5t4M.x -p x --cpu 2
-exit
